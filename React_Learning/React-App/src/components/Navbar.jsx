c50fad9029cfdb94cbb9df38f0301e4f95fdd8a7
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-12-18 17:37:39 +0530

    create a new folder to practice the components

---
+++ React_Learning/React-App/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar=()=>{
+
+    return(
+        <>
+
+        {/* Top Navbar */}
+        <div className=' flex fixed  left-0 right-0 justify-evenly p-2 bg-blue-300 font-bold text-white'>
+             <h1>Home</h1>
+             <h1>Menu</h1>
+             <h1>Contact</h1>
+        </div>
+        
+        </>
+    );
+}
+
+export default Navbar
